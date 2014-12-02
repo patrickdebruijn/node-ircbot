a4a69eea8dd5c4f1089792f16d185939afe9e91b
Patrick <patrickdebruijn@gmail.com>
2014-12-02 21:41:57 +0100

    commit

--- ircNode/lib/ircClient/engine.js
+++ ircNode/lib/ircClient/engine.js
@@ -56,7 +56,7 @@ connectDB = function () {
     if (!state.dbConnected) {
 
         if(process.env.MONGOLAB_URI !=undefined)
-            var dburl= process.env.MONGOLAB_URI;
+            var dburl= process.env.MONGOLAB_URI.replace("mongodb://","");
         else
             var dburl= 'nodeIrcBotDb';
         eng.logThis('debug', 'Connecting to: ' + dburl,'CONNECTDB');
